f4c765148d441714fe2b47c52b43b85effcd5965
Govardhan Reddy Morsu <morsu.gr000@gmail.com>
2022-10-03 16:36:35 +0530

    Layout changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
-import { ViewDetailsComponent } from './admin/view-details/view-details.component';
-import { PenrequestComponent } from './admin/penrequest/penrequest.component';
-import { RegistrationpageComponent } from './Authentication/registrationpage/registrationpage.component';
-<<<<<<< HEAD
-import { ReportpageComponent } from './reportpage/reportpage.component';
-=======
-import { LdbpageComponent } from './admin/ldbpage/ldbpage.component';
-import { ReceivablesComponent } from './admin/receivables/receivables.component';
-import { ForgetpasswordComponent } from './Authentication/forgetpassword/forgetpassword.component';
-import { VisaupdateComponent } from './admin/visaupdate/visaupdate.component';
->>>>>>> 3e2395963f4d768efae17f75c50f25155ebf1e24
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthenticationLayoutComponent } from './layouts/authentication-layout/authentication-layout.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PenrequestComponent,
-    ViewDetailsComponent,
-    RegistrationpageComponent,
-<<<<<<< HEAD
-    ReportpageComponent,
-    
-=======
-    LdbpageComponent,
-    ReceivablesComponent,
-    ForgetpasswordComponent,
-    VisaupdateComponent
->>>>>>> 3e2395963f4d768efae17f75c50f25155ebf1e24
-
+    AuthenticationLayoutComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-   AppRoutingModule
-
-    
-=======
     AppRoutingModule,
-   
-
->>>>>>> 3e2395963f4d768efae17f75c50f25155ebf1e24
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
