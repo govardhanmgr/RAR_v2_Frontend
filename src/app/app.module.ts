664416b241fa5ee44976a68d5132c8978835d38f
poorna sai <ramisetti.poornasai@gamil.com>
2022-10-03 16:23:48 +0530

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import { ViewDetailsComponent } from './admin/view-details/view-details.component';
 import { PenrequestComponent } from './admin/penrequest/penrequest.component';
 import { RegistrationpageComponent } from './Authentication/registrationpage/registrationpage.component';
-
-
-
 import { LdbpageComponent } from './admin/ldbpage/ldbpage.component';
 import { ReceivablesComponent } from './admin/receivables/receivables.component';
 import { ForgetpasswordComponent } from './Authentication/forgetpassword/forgetpassword.component';
