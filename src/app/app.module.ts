1b5865801b08ccf9b173ccc9409b45326bd1818c
channupraveen <channupraveen66@gmail.com>
2022-10-03 14:50:59 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -8,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ViewDetailsComponent } from './admin/view-details/view-details.component';
 import { PenrequestComponent } from './admin/penrequest/penrequest.component';
 import { RegistrationpageComponent } from './Authentication/registrationpage/registrationpage.component';
+import { ReportpageComponent } from './reportpage/reportpage.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +15,15 @@ import { RegistrationpageComponent } from './Authentication/registrationpage/reg
     PenrequestComponent,
     ViewDetailsComponent,
     RegistrationpageComponent,
+    ReportpageComponent,
+    
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+   AppRoutingModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
