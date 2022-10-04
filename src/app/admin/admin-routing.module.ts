a71d2133fa0663f0e143cd479698c08b13b489f6
susmitha28 <saisusmitha28@gmail.com>
2022-10-04 13:38:56 +0530

    modify

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,12 +4,14 @@ import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.compo
 import { DepartmentComponent } from './department/department.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { LaunchpageComponent } from './launchpage/launchpage.component';
+import { VisaupdateComponent } from './visaupdate/visaupdate.component';
 
 const routes: Routes = [
   { path: '', component: AdminDashboardComponent },
   { path: 'employee', component: EmployeeComponent },
   { path: 'department', component: DepartmentComponent },
   { path: 'launchpage', component: LaunchpageComponent },
+  
 ];
 
 @NgModule({
