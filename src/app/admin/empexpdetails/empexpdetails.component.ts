7bd09dac23fae0f0fc897f32a12324dbbe15d0fc
Mudassir0978 <mdmudassir354@gmail.com>
2022-10-04 15:48:03 +0530

    update

---
+++ src/app/admin/empexpdetails/empexpdetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empexpdetails',
+  templateUrl: './empexpdetails.component.html',
+  styleUrls: ['./empexpdetails.component.css']
+})
+export class EmpexpdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
