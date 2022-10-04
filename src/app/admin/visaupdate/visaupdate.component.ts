e64259c92f6fac5fcf6d396cf9495fe2c7a9e1cb
susmitha28 <saisusmitha28@gmail.com>
2022-10-04 09:41:24 +0530

    Component

---
+++ src/app/admin/visaupdate/visaupdate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visaupdate',
+  templateUrl: './visaupdate.component.html',
+  styleUrls: ['./visaupdate.component.css']
+})
+export class VisaupdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
