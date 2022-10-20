fe18ce11a474ec3cc8d1e366175c41cedbe635f0
Rohinireddy <chinnirohinireddy@gmail.com>
2022-10-20 12:57:30 +0530

    update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { AdminFooterComponent } from './admin-footer/admin-footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AdminHeaderComponent,
-    AdminFooterComponent
+    AdminFooterComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
