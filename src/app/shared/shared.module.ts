b40e83801e9042ccdb0993a0703929e1df4254ee
Girish <kolliparagirish61@gmail.com>
2022-10-20 12:41:27 +0530

    update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,17 +4,19 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { AdminFooterComponent } from './admin-footer/admin-footer.component';
+import { AmdbuttonsComponent } from './amdbuttons/amdbuttons.component';
 
 
 @NgModule({
   declarations: [
     AdminHeaderComponent,
-    AdminFooterComponent
+    AdminFooterComponent,
+    AmdbuttonsComponent
   ],
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
-  exports: [AdminHeaderComponent, AdminFooterComponent]
+  exports: [AdminHeaderComponent, AdminFooterComponent,AmdbuttonsComponent]
 })
 export class SharedModule { }
