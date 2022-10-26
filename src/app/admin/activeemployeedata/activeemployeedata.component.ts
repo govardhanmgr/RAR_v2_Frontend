6ecee32652445e77ff2b42cb63acd7fa9a17060d
Govardhan Reddy <morsu.gr000.com>
2022-10-26 15:05:54 +0530

    routerliks updated

--- src/app/admin/activeemployeedata/activeemployeedata.component.ts
+++ src/app/admin/activeemployeedata/activeemployeedata.component.ts
@@ -1,34 +1,129 @@
 import { Component, OnInit } from '@angular/core';
-import { IActive } from './activeemployeedata';
 
 @Component({
   selector: 'app-activeemployeedata',
   templateUrl: './activeemployeedata.component.html',
-  styleUrls: ['./activeemployeedata.component.css']
+  styleUrls: ['./activeemployeedata.component.css'],
 })
 export class ActiveemployeedataComponent implements OnInit {
-  actemployees=[]as IActive[];
+  actemployees = [] as any[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.actemployees=[
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12345',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12344',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12343',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12341',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12348',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'},
-      {actEmployeeNumber:'12346',actFirstName:'koushik',actLastName:'anumasa',actEmail:'koushik@gmail.com',actContactNumber:12345679,actRole:'snad'}
-      
-    ]
+    this.actemployees = [
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12345',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12344',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12343',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12341',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12348',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+      {
+        actEmployeeNumber: '12346',
+        actFirstName: 'koushik',
+        actLastName: 'anumasa',
+        actEmail: 'koushik@gmail.com',
+        actContactNumber: 12345679,
+        actRole: 'snad',
+      },
+    ];
   }
-
 }
