923f40dfa019c53b0466f857033319c9ce57b301
Santhoshkumarale <santhoshkumarale401@gmail.com>
2022-11-01 11:44:54 +0530

    changes

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpService } from 'src/app/services/http.service';
 import { Isignup } from '../register-model';
 
 
@@ -10,15 +13,16 @@ import { Isignup } from '../register-model';
 })
 export class SignupComponent implements OnInit {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-
+ 
   Signup = { } as Isignup;
-  constructor() { }
-
+  
+  constructor(private _httpservice:HttpService,
+    private router:Router,) { }
+  subscription!: Subscription;
   ngOnInit(): void {
   }
  
   register(f:NgForm){
-        console.log(this.Signup);
-    
+   
   }
 }
