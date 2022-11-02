0db90f97b39e5bcc219bbb138d1cc71d19aa32ba
susmitha28 <susmitha28@gmail.com>
2022-11-02 17:07:30 +0530

    update

--- src/app/admin/personaldata/personaldata.component.ts
+++ src/app/admin/personaldata/personaldata.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpService } from 'src/app/services/http.service';
 import { Ipersonaldata } from './personal-model';
 
 @Component({
@@ -7,14 +10,60 @@ import { Ipersonaldata } from './personal-model';
   templateUrl: './personaldata.component.html',
   styleUrls: ['./personaldata.component.css']
 })
-export class PersonaldataComponent implements OnInit {
+export class PersonaldataComponent implements OnInit, OnDestroy {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  educationData=[] as any;
+  visaData=[] as any;
   PERSONAL= {} as   Ipersonaldata;
-  constructor() { }
+  empdata ={} as any;
+  subscription !:Subscription;
+
+
+  constructor(
+    private http:HttpService,
+    private router:Router
+  ) { }
+ 
 
   ngOnInit(): void {
+    this.empdata = JSON.parse(localStorage.getItem("personaldata") || '{}')
+    console.log(this.empdata)
+
+    this.getEducationData();
+    this.getvisainformation();
+  }
+
+
+  getEducationData(){
+    let sdata = {
+      "employeeid": parseInt(this.empdata.employeeid)
+    }
+    console.log(sdata)
+    this.subscription = this.http.postdata("geteducation", sdata).subscribe({
+      next:(data:any)=>{
+        console.log(data);
+        this.educationData= data
+      }
+    })
+  }
+  getvisainformation(){
+    let sdata = {
+      "employeeid": parseInt(this.empdata.employeeid)
+    }
+    console.log(sdata)
+    this.subscription = this.http.postdata("getvisainformation", sdata).subscribe({
+      next:(data:any)=>{
+        console.log(data);
+        this.visaData= data
+      }
+    })
   }
   personal(f:NgForm){
     
+  }
+  ngOnDestroy(): void {
+    if(this.subscription){
+      this.subscription.unsubscribe();
+    }
   }
 }
--- src/app/admin/visaupdate/visaupdate.component.ts
+++ src/app/admin/visaupdate/visaupdate.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,NgForm,Validators,FormGroupDirective } from '@angular/forms';
+import { FormControl, NgForm, Validators, FormGroupDirective } from '@angular/forms';
+import { Router } from '@angular/router';
+import * as moment from 'moment';
 import { Subscription } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
 import { Ivisaupdate } from './visa-model';
@@ -11,28 +13,62 @@ import { Ivisaupdate } from './visa-model';
 })
 export class VisaupdateComponent implements OnInit {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  constructor(private http:HttpService) { }
+
+  empdata = {} as any;
+  constructor(
+    private http: HttpService,
+    private router:Router
+    ) { }
   subscription!: Subscription;
-  visaupdate=[] as any;
-  VISA={} as Ivisaupdate;
+
+  VISA = {} as Ivisaupdate;
 
   ngOnInit(): void {
-    this.visainfo();
+    this.empdata = JSON.parse(localStorage.getItem("personaldata") || '{}')
+    console.log(this.empdata)
   }
 
-  Visupdate(f: NgForm){
-    
-  }
+  visaupdate(f: NgForm) {
+    var expirydate = moment(this.VISA.expirationdate);
+    var status;
 
+    var now = moment();
 
-    visainfo(){
-      this.subscription=this.http.getData("getvisainformation").subscribe({
-        next: data  => {
-            this.visaupdate=data
-        }
-      })
-      console.log(this.visaupdate)
+    if (now > expirydate) {
+      console.log("past")
+      status = "Inactive"
+    } else {
+      console.log("future")
+      status = "Active"
     }
+
+
+
+
+    this.VISA.employeeid = parseInt(this.empdata.employeeid)
+    this.VISA.status = status
+    console.log(this.VISA)
+
+
+    this.subscription = this.http.empPostData("empdata/", this.VISA, parseInt(this.empdata.id)).subscribe({
+      next: (data: any) => {
+        console.log(data)
+        if(data.statuscode==200){
+          alert(data.message)
+          this.router.navigate(["/admin/personaldata"])
+        }
+      },
+      error: (reason: any) => {
+        console.log(reason);
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    if(this.subscription)
+    this.subscription.unsubscribe();
   }
 
+
+
+}
 
