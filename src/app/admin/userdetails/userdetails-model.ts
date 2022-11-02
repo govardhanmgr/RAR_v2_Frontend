9c41d87d05d9e99f5ea9dc0f990d1dddad9385ab
MOHAMMED MUDASSIR <mdmudassir354@gmail.com>
2022-11-02 11:38:48 +0530

    update

---
+++ src/app/admin/userdetails/userdetails-model.ts
@@ -0,0 +1,21 @@
+export interface IUserDetails {
+    
+        firstName: string;
+        lstName:  string;
+        employeeId:  number;
+        emailId:  string;
+        phoneNumber:  number;
+        gender: string;
+        role:  string;
+        addressLine1:  string;
+        addressLine2:  string;
+        country:  string;
+        state:  string;
+        city:  string;
+        zipcode:  number;
+        password:  string;
+        confirmPassword:  string;
+        entity:string;
+    }
+
+ 
