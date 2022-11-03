605c526f3621e1fba1abdc8d1b4a3de2ead96e0c
mounikaboinapally <mounika.boinapally12@gmail.com>
2022-11-03 17:36:32 +0530

    details updated

--- src/app/admin/employee-expensedata/employee-expensedatamodul.ts
+++
@@ -1,6 +0,0 @@
-export interface IEmployeeExpensedata {
-  employeeid: number;
-  firstname: string;
-  lastname: string;
-  sum:number;
-  }
--- src/app/admin/viewdetails/viewdetails.component.ts
+++ src/app/admin/viewdetails/viewdetails.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpService } from 'src/app/services/http.service';
+import { IViewdetails } from './viewdetails-model';
 
 @Component({
   selector: 'app-viewdetails',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viewdetails.component.css']
 })
 export class ViewdetailsComponent implements OnInit {
-
-  constructor() { }
+  viewdetails= {} as any;
+ 
+ subscription!: Subscription; 
+  constructor(private http:HttpService,
+    private router:Router) { }
+ 
 
   ngOnInit(): void {
+    this.viewdetails=JSON.parse(localStorage.getItem('updatedata')||'{}');
+    console.log(this.viewdetails)
+    // this.Viewdetail();
   }
+  // Viewdetail(){
+   
+  //   this.subscription = this.http.getData("updatedata").subscribe({
+  //     next: (data: any) => {
+  //       this.viewdetails = data.result as IViewdetails [];
+  //       console.log(this.viewdetails)
+  //     },
+  //   });
+ 
+  // }
 
-}
+    ngOnDestroy(): void {
+      if(this.subscription)
+      this.subscription.unsubscribe();
+    }
+  }
