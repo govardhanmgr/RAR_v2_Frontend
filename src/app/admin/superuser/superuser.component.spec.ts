315bc80fef610fd53efb5cb1be2b6c10d10ad9da
susmitha28 <saisusmitha28@gmail.com>
2022-11-29 15:05:07 +0530

    commit

--- src/app/admin/superuser/superuser.component.spec.ts
+++ src/app/admin/superuser/superuser.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdminComponent } from './admin.component';
+import { SuperuserComponent } from './superuser.component';
 
-describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
+describe('SuperuserComponent', () => {
+  let component: SuperuserComponent;
+  let fixture: ComponentFixture<SuperuserComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [ SuperuserComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AdminComponent);
+    fixture = TestBed.createComponent(SuperuserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
